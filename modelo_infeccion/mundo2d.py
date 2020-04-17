f6ce28d199fe0713b9b175ab0174db5c0d894a89
Diego Prada <prada.gracia@gmail.com>
2020-04-17 14:08:21 -0500

    terminando resultados para la charla

--- modelo_infeccion/mundo2d.py
+++ modelo_infeccion/mundo2d.py
@@ -16,6 +16,7 @@ class Mundo2d():
         self.ocupacion = ocupacion
         self.matriz = np.zeros((n_filas,n_columnas), dtype = int)
         self._aleatorizacion()
+        self._promedio_vecinos()
 
     def _aleatorizacion(self):
 
@@ -49,6 +50,45 @@ class Mundo2d():
         for i in range(self.n_filas): # para la columna 0-ésimo
             if self.matriz[i,0] ==1:
                 self.matriz[i,0] =2
+
+    def ciclo_propagacion(self):
+
+        for j in range(1,self.n_columnas-1): #propagación para el resto de columnas
+
+            #Propagación mirando a j-1
+            if self.matriz[0,j] == 1:
+                if (self.matriz[0,j-1]==2) or (self.matriz[1,j-1]==2)\
+                or (self.matriz[1,j]==2) or (self.matriz[1,j+1]==2) or (self.matriz[0,j+1]==2):
+                    self.matriz[0,j] = 2
+
+            for i in range(1,self.n_filas-1):
+                if self.matriz[i,j] == 1:
+                    if (self.matriz[i-1,j-1]==2) or (self.matriz[i,j-1]==2) or (self.matriz[i+1,j-1]==2)\
+                    or (self.matriz[i+1,j]==2) or (self.matriz[i-1,j]==2)\
+                    or (self.matriz[i-1,j+1]==2) or (self.matriz[i,j+1]==2) or (self.matriz[i+1,j+1]==2):
+                        self.matriz[i,j] = 2
+
+            if self.matriz[self.n_filas-1,j] == 1:
+                if (self.matriz[self.n_filas-1,j-1] == 2) or (self.matriz[self.n_filas-2,j-1] == 2)\
+                or (self.matriz[self.n_filas-2,j]==2) or (self.matriz[self.n_filas-1,j+1] == 2) or (self.matriz[self.n_filas-2,j+1] == 2):
+                    self.matriz[self.n_filas-1,j] = 2
+
+        if self.matriz[0,self.n_columnas-1] == 1:
+            if (self.matriz[0,self.n_columnas-2]==2) or (self.matriz[1,self.n_columnas-2]==2)\
+            or (self.matriz[1,self.n_columnas-1]==2):
+                self.matriz[0,self.n_columnas-1] = 2
+
+        for i in range(1,self.n_filas-1):
+            if self.matriz[i,self.n_columnas-1] == 1:
+                if (self.matriz[i-1,self.n_columnas-2]==2) or (self.matriz[i,self.n_columnas-2]==2) or (self.matriz[i+1,self.n_columnas-2]==2)\
+                or (self.matriz[i+1,self.n_columnas-1]==2) or (self.matriz[i-1,self.n_columnas-1]==2):
+                    self.matriz[i,self.n_columnas-1] = 2
+
+        if self.matriz[self.n_filas-1,self.n_columnas-1] == 1:
+            if (self.matriz[self.n_filas-1,self.n_columnas-2] == 2) or (self.matriz[self.n_filas-2,self.n_columnas-2] == 2)\
+            or (self.matriz[self.n_filas-2,self.n_columnas-1]==2):
+                self.matriz[self.n_filas-1,self.n_columnas-1] = 2
+
 
 
     def propagacion(self):
@@ -102,6 +142,29 @@ class Mundo2d():
                     nuevos_infectados=True
 
         self._obtener_porcentaje_infectado()
+
+    def _promedio_vecinos(self):
+
+        poblacion=0
+        vecinos=0
+
+        for j in range(1,self.n_columnas-1): #propagación para el resto de columnas
+            for i in range(1,self.n_filas-1):
+                if self.matriz[i,j]==1:
+                    poblacion+=1
+                    if self.matriz[i,j-1]==1: vecinos+=1
+                    if self.matriz[i,j+1]==1: vecinos+=1
+                    if self.matriz[i-1,j]==1: vecinos+=1
+                    if self.matriz[i+1,j]==1: vecinos+=1
+                    if self.matriz[i-1,j-1]==1: vecinos+=1
+                    if self.matriz[i+1,j+1]==1: vecinos+=1
+                    if self.matriz[i-1,j+1]==1: vecinos+=1
+                    if self.matriz[i+1,j-1]==1: vecinos+=1
+
+        if poblacion==0:
+            self.vecinos_promedio = 0.0
+        else:
+            self.vecinos_promedio = (vecinos*1.0)/(poblacion*1.0)
 
 
     def _obtener_porcentaje_infectado(self):
